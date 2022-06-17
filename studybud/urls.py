a9539b69fcb810d6ecad9fb7799859e01b8b16f7
Riverside96 <riverside96@protonmail.com>
2022-06-17 11:27:06 +0100

    + API

--- studybud/urls.py
+++ studybud/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('api/', include('base.api.urls'))  #any url starting with api, -> send them to the include
 
 ]
